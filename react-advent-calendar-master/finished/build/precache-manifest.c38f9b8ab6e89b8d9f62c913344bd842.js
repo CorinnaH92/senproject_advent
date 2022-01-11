327b0928be7c9cebc4e9261ac0ea26f082b22763
Corinna <corinna.hartl@htl-klu.at>
2022-01-11 18:21:06 +0100

    Fast fertig

---
+++ react-advent-calendar-master/finished/build/precache-manifest.c38f9b8ab6e89b8d9f62c913344bd842.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f896b707fd9a50faf7c4174125611fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40b64a6137f19c264d1b",
+    "url": "/static/js/2.a18134f5.chunk.js"
+  },
+  {
+    "revision": "b7fb09b6b7d095219ab7",
+    "url": "/static/js/main.ff23b05b.chunk.js"
+  },
+  {
+    "revision": "db927b1f5992b42704f8",
+    "url": "/static/js/runtime-main.2afe415d.js"
+  },
+  {
+    "revision": "fe63257737c2cbb1bd873201c675fb40",
+    "url": "/static/media/hatch_backdrop.fe632577.jpg"
+  }
+]);
